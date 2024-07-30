a8c3f1aa7fa089b84e2e0f3815cd86beca98c39a
ghinzuka <baptiste.pouquerou@gmail.com>
2024-07-30 16:27:34 +0200

    finished the task 1 and it work on the app.py

--- mock-api/static/scripts/script.js
+++ mock-api/static/scripts/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const password = document.getElementById('password').value;
 
             try {
-                const response = await fetch('http://localhost:5000/login', {
+                const response = await fetch('http://127.0.0.1:5000/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
